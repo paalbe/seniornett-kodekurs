8908c25fcf4d68afa3ee2b6da4a9238f3994130a
Pål Bergquist <pal.bergquist@gmail.com>
2019-12-21 13:50:22 +0100

    Initial commit

---
+++ js/kalkulator.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', function () {
+    document.querySelector("#idpluss").addEventListener('click', function () {
+        var svar = parseInt(document.querySelector("#idtalla").value) 
+                   + parseInt(document.querySelector("#idtallb").value);
+        document.querySelector("#idsvar").innerHTML = svar;
+    });
+    document.querySelector("#idminus").addEventListener('click', function () {
+        var svar = parseInt(document.querySelector("#idtalla").value) 
+                   - parseInt(document.querySelector("#idtallb").value);
+        document.querySelector("#idsvar").innerHTML = svar;
+    });
+    document.querySelector("#idmult").addEventListener('click', function () {
+        var svar = parseInt(document.querySelector("#idtalla").value) 
+                   * parseInt(document.querySelector("#idtallb").value);
+        document.querySelector("#idsvar").innerHTML = svar;
+    });
+    document.querySelector("#iddivi").addEventListener('click', function () {
+        var svar = parseInt(document.querySelector("#idtalla").value) 
+                   / parseInt(document.querySelector("#idtallb").value);
+        svar = svar.toFixed(2);
+        document.querySelector("#idsvar").innerHTML = svar;
+    });            
+});
